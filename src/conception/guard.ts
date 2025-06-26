04039c35ad08da7d6488da15448df38bcd7a9a6e
Ivan Platunov <i@iplatunov.ru>
2025-06-27 01:14:59 +0300

    users

--- src/conception/guard.ts
+++ src/conception/guard.ts
@@ -3,9 +3,8 @@ import {CanActivate, ExecutionContext, Injectable, UnauthorizedException} from "
 @Injectable()
 export class AuthGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean {
-        console.log('guard');
         const request = context.switchToHttp().getRequest();
-        const isAuth = false;
+        const isAuth = true;
         if (!isAuth) {
             throw new UnauthorizedException("Not authorized user!");
         }
